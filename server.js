a0bcd657ba536027445be6a3643befbf259e489e
Braydon Nelson <braydon.nelson@outlook.com>
2020-02-27 23:06:58 -0700

    added server.js to link correctly

---
+++ server.js
@@ -0,0 +1,29 @@
+var express = require("express");
+var exphbs = require("express-handlebars");
+
+var app = express();
+
+// Set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var PORT = process.env.PORT || 8080;
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
+
+// Parse application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+var burger = require('./controllers/burgers_ontroller.js')
+
+//Created a simple router with express router from the burgersControllers.js script
+app.use(burger)
+
+// Start our server so that it can begin listening to client requests.
+app.listen(PORT, function () {
+    // Log (server-side) when our server has started
+    console.log("Server listening on: http://localhost:" + PORT);
+});
