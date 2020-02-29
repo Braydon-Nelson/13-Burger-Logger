9c38d0aeb2c4eb81102d157e54e0e9cafc2873fa
Braydon Nelson <braydon.nelson@outlook.com>
2020-02-29 12:01:11 -0700

    test of removeing route

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,9 +1,9 @@
 var express = require('express')
-var router = express.Router()
+const app = express();
 var burger = require('../models/burger.js');
 
 //homepage route to render body of page
-router.get('/', function (req, res) {
+app.get('/', function (req, res) {
     burger.selectAll(function (data) {
         var handlebarObject = { burgers: data };
         console.log(handlebarObject);
@@ -14,21 +14,21 @@ router.get('/', function (req, res) {
 });
 
 //route for creating a burger
-router.post('/burger/create', function (req, res) {
+app.post('/burger/create', function (req, res) {
     burger.insertOne(req.body.burger_name, function () {
         res.redirect('/');
     });
 });
 
 //route for 'eating' a burger
-router.post('/burger/eat', function (req, res) {
+app.post('/burger/eat', function (req, res) {
     burger.updateOne(req.body.id, function () {
         res.redirect('/');
     });
 });
 
 //route for deleting a burger
-router.post('/burger/delete', function (req, res) {
+app.post('/burger/delete', function (req, res) {
     burger.deleteOne(req.body.id, function () {
         res.redirect('/');
     });
